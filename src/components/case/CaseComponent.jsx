b768b5febee6bc6e15fd6537a6f07e5e15dcd3e9
oscar19940516 <oscar19940516@gmail.com>
2023-06-15 02:37:04 +0900

    padding remove

--- src/components/case/CaseComponent.jsx
+++ src/components/case/CaseComponent.jsx
@@ -155,7 +155,7 @@ const cesdkWrapperStyle = {
   overflow: 'hidden',
   flexGrow: 1,
   display: 'flex',
-  borderRadius: '0.75rem',
+  // borderRadius: '0.75rem',
   boxShadow:
     '0px 0px 2px rgba(0, 0, 0, 0.25), 0px 18px 18px -2px rgba(18, 26, 33, 0.12), 0px 7.5px 7.5px -2px rgba(18, 26, 33, 0.12), 0px 3.75px 3.75px -2px rgba(18, 26, 33, 0.12)',
   minHeight: '740px'
